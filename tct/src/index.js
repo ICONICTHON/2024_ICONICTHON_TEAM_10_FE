88f028da23df1f8c4967e094dd1130b098215c14
awrgawa <bartas21@naver.com>
2024-11-14 20:56:23 +0900

    Start button presssed in Main Screen

--- tct/src/index.js
+++ tct/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './Reservation.js';
+import App from './ReservationInfo.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
